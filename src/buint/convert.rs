aa2dbf6cd4900ed3c1e2c6337e9fc975070401e0
larry <26318510+larry0x@users.noreply.github.com>
2024-06-06 12:26:23 +0100

    move buint conversion from types.rs to buint/convert.rs

--- src/buint/convert.rs
+++ src/buint/convert.rs
@@ -261,3 +261,81 @@ macro_rules! convert {
 }
 
 crate::macro_impl!(convert);
+
+// ---------------------------- grug added content -----------------------------
+
+use crate::{BInt, BUint};
+use crate::types::{U256, U512};
+
+macro_rules! big_conversion {
+    (
+        $from:tt => $to:tt
+    ) => {
+        impl From<$from> for U512 {
+            fn from(value: $from) -> Self {
+                const FROM_BYTES_LEN: usize = <$from>::BYTES as usize;
+                const TO_BYTES_LEN: usize = <$to>::BYTES as usize;
+
+                // --- value.to_le_bytes() ---
+
+                let words = value.digits();
+                let mut bytes: [[u8; 8]; FROM_BYTES_LEN / 8] = [[0u8; 8]; FROM_BYTES_LEN / 8];
+                for i in 0..FROM_BYTES_LEN / 8 {
+                    bytes[i] = words[i].to_le_bytes();
+                }
+
+                let from_bytes: [u8; FROM_BYTES_LEN] = unsafe { core::mem::transmute(bytes) };
+                let mut to_bytes = [0_u8; TO_BYTES_LEN];
+                to_bytes[..FROM_BYTES_LEN].copy_from_slice(&from_bytes);
+
+                // --- Value from le bytes ---
+
+                let mut bytes = [0u64; TO_BYTES_LEN / 8];
+                for i in 0..TO_BYTES_LEN / 8 {
+                    bytes[i] = u64::from_le_bytes([
+                        to_bytes[i * 8],
+                        to_bytes[i * 8 + 1],
+                        to_bytes[i * 8 + 2],
+                        to_bytes[i * 8 + 3],
+                        to_bytes[i * 8 + 4],
+                        to_bytes[i * 8 + 5],
+                        to_bytes[i * 8 + 6],
+                        to_bytes[i * 8 + 7],
+                    ])
+                }
+                Self::from_digits(bytes)
+            }
+        }
+
+        impl TryFrom<$to> for $from {
+            type Error = TryFromIntError;
+            fn try_from(value: $to) -> Result<Self, Self::Error> {
+                BTryFrom::<$to>::try_from(value)
+            }
+        }
+    };
+}
+
+big_conversion!(U256 => U512);
+
+#[test]
+fn test_from_to() {
+    let u256 = U256::from(42_u64);
+    let u512: U512 = u256.into();
+    assert_eq!(u512, U512::from(42_u64));
+
+    let u256: U256 = TryFrom::<U512>::try_from(u512).unwrap();
+    assert_eq!(u256, U256::from(42_u64));
+
+    let u256: Result<U256, TryFromIntError> = TryFrom::<U512>::try_from(U512::MAX);
+    assert!(u256.is_err());
+
+    let u256 = U256::MAX;
+    let mut u512: U512 = u256.into();
+    let _: U256 = TryFrom::<U512>::try_from(u512).unwrap();
+
+    u512 += U512::ONE;
+
+    let u256: Result<U256, TryFromIntError> = TryFrom::<U512>::try_from(U512::MAX);
+    assert!(u256.is_err());
+}
--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,6 @@
 //! Type aliases for big signed and unsigned integers. Each is an alias for either a [`BUint`] or a [`BInt`].
 
-use crate::{errors::TryFromIntError, BInt, BTryFrom, BUint};
+use crate::{BInt, BUint};
 
 macro_rules! int_type_doc {
     ($bits: literal, $sign: literal) => {
@@ -34,62 +34,12 @@ macro_rules! call_types_macro {
     };
 }
 
-macro_rules! big_conversion {
-    (
-        $from:tt => $to:tt
-    ) => {
-        impl From<$from> for U512 {
-            fn from(value: $from) -> Self {
-                const FROM_BYTES_LEN: usize = <$from>::BYTES as usize;
-                const TO_BYTES_LEN: usize = <$to>::BYTES as usize;
-
-                // --- value.to_le_bytes() ---
-
-                let words = value.digits();
-                let mut bytes: [[u8; 8]; FROM_BYTES_LEN / 8] = [[0u8; 8]; FROM_BYTES_LEN / 8];
-                for i in 0..FROM_BYTES_LEN / 8 {
-                    bytes[i] = words[i].to_le_bytes();
-                }
-
-                let from_bytes: [u8; FROM_BYTES_LEN] = unsafe { core::mem::transmute(bytes) };
-                let mut to_bytes = [0_u8; TO_BYTES_LEN];
-                to_bytes[..FROM_BYTES_LEN].copy_from_slice(&from_bytes);
-
-                // --- Value from le bytes ---
-
-                let mut bytes = [0u64; TO_BYTES_LEN / 8];
-                for i in 0..TO_BYTES_LEN / 8 {
-                    bytes[i] = u64::from_le_bytes([
-                        to_bytes[i * 8],
-                        to_bytes[i * 8 + 1],
-                        to_bytes[i * 8 + 2],
-                        to_bytes[i * 8 + 3],
-                        to_bytes[i * 8 + 4],
-                        to_bytes[i * 8 + 5],
-                        to_bytes[i * 8 + 6],
-                        to_bytes[i * 8 + 7],
-                    ])
-                }
-                Self::from_digits(bytes)
-            }
-        }
-
-        impl TryFrom<$to> for $from {
-            type Error = TryFromIntError;
-            fn try_from(value: $to) -> Result<Self, Self::Error> {
-                BTryFrom::<$to>::try_from(value)
-            }
-        }
-    };
-}
-
-big_conversion!(U256 => U512);
-
 call_types_macro!(int_types);
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
     macro_rules! assert_int_bits {
         { $($bits: literal $u: ident $i: ident; ) *} => {
             $(
@@ -102,27 +52,5 @@ mod tests {
     #[test]
     fn test_int_bits() {
         call_types_macro!(assert_int_bits);
-    }
-
-    #[test]
-    fn test_from_to() {
-        let u256 = U256::from(42_u64);
-        let u512: U512 = u256.into();
-        assert_eq!(u512, U512::from(42_u64));
-
-        let u256: U256 = TryFrom::<U512>::try_from(u512).unwrap();
-        assert_eq!(u256, U256::from(42_u64));
-
-        let u256: Result<U256, TryFromIntError> = TryFrom::<U512>::try_from(U512::MAX);
-        assert!(u256.is_err());
-
-        let u256 = U256::MAX;
-        let mut u512: U512 = u256.into();
-        let _: U256 = TryFrom::<U512>::try_from(u512).unwrap();
-
-        u512 += U512::ONE;
-
-        let u256: Result<U256, TryFromIntError> = TryFrom::<U512>::try_from(U512::MAX);
-        assert!(u256.is_err());
     }
 }
