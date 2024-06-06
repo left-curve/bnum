91dfe8a34402c11f3ecae8b0eeab5f091f0aa2fb
larry <26318510+larry0x@users.noreply.github.com>
2024-06-06 15:13:48 +0100

    updates

--- src/buint/convert.rs
+++ src/buint/convert.rs
@@ -264,51 +264,40 @@ crate::macro_impl!(convert);
 
 // ---------------------------- grug added content -----------------------------
 
-use crate::{BInt, BUint};
 use crate::types::{U256, U512};
 
+/// Provides compile time check that the first given number is less than the
+/// second given number.
+const fn ensure_a_less_than_b(a: u32, b: u32) {
+    assert!(a < b);
+}
+
+/// Implements `From` and `TryFrom` traits between two `BUint` types.
+///
+/// The `from` type must have fewer digits than the `to` types. For example,
+/// `U256 => U512` is valid but `U512 => U256` isn't. This is checked at
+/// compile time.
 macro_rules! big_conversion {
-    (
-        $from:tt => $to:tt
-    ) => {
-        impl From<$from> for U512 {
+    ($from:tt => $to:tt) => {
+        // Make sure at compile time that `$from` is less than `$to`.
+        const _: () = ensure_a_less_than_b($from::BYTES, $to::BYTES);
+
+        impl From<$from> for $to {
             fn from(value: $from) -> Self {
-                const FROM_BYTES_LEN: usize = <$from>::BYTES as usize;
-                const TO_BYTES_LEN: usize = <$to>::BYTES as usize;
+                // Note: In bnum, the digit is `u64`, even in 32-bit systems.
+                // The number of digits is the number of bytes divided by 8.
+                const FROM_DIGITS: usize = $from::BYTES as usize / 8;
+                const TO_DIGITS: usize = $to::BYTES as usize / 8;
 
-                // --- value.to_le_bytes() ---
-
-                let words = value.digits();
-                let mut bytes: [[u8; 8]; FROM_BYTES_LEN / 8] = [[0u8; 8]; FROM_BYTES_LEN / 8];
-                for i in 0..FROM_BYTES_LEN / 8 {
-                    bytes[i] = words[i].to_le_bytes();
-                }
-
-                let from_bytes: [u8; FROM_BYTES_LEN] = unsafe { core::mem::transmute(bytes) };
-                let mut to_bytes = [0_u8; TO_BYTES_LEN];
-                to_bytes[..FROM_BYTES_LEN].copy_from_slice(&from_bytes);
-
-                // --- Value from le bytes ---
-
-                let mut bytes = [0u64; TO_BYTES_LEN / 8];
-                for i in 0..TO_BYTES_LEN / 8 {
-                    bytes[i] = u64::from_le_bytes([
-                        to_bytes[i * 8],
-                        to_bytes[i * 8 + 1],
-                        to_bytes[i * 8 + 2],
-                        to_bytes[i * 8 + 3],
-                        to_bytes[i * 8 + 4],
-                        to_bytes[i * 8 + 5],
-                        to_bytes[i * 8 + 6],
-                        to_bytes[i * 8 + 7],
-                    ])
-                }
-                Self::from_digits(bytes)
+                let mut digits = [0; TO_DIGITS];
+                digits[..FROM_DIGITS].copy_from_slice(value.digits());
+                Self::from_digits(digits)
             }
         }
 
         impl TryFrom<$to> for $from {
             type Error = TryFromIntError;
+
             fn try_from(value: $to) -> Result<Self, Self::Error> {
                 BTryFrom::<$to>::try_from(value)
             }
@@ -318,24 +307,26 @@ macro_rules! big_conversion {
 
 big_conversion!(U256 => U512);
 
-#[test]
-fn test_from_to() {
-    let u256 = U256::from(42_u64);
-    let u512: U512 = u256.into();
-    assert_eq!(u512, U512::from(42_u64));
+// ----------------------------------- tests -----------------------------------
 
-    let u256: U256 = TryFrom::<U512>::try_from(u512).unwrap();
-    assert_eq!(u256, U256::from(42_u64));
+#[cfg(test)]
+mod tests {
+    use crate::types::{U256, U512};
+    use core::convert::TryFrom;
 
-    let u256: Result<U256, TryFromIntError> = TryFrom::<U512>::try_from(U512::MAX);
-    assert!(u256.is_err());
+    #[test]
+    fn converting_big_uint() {
+        // Casting a small number of U256 to U512, should work.
+        let u256 = U256::from(42u64);
+        let u512: U512 = u256.into();
+        assert_eq!(u512, U512::from(42u64));
 
-    let u256 = U256::MAX;
-    let mut u512: U512 = u256.into();
-    let _: U256 = TryFrom::<U512>::try_from(u512).unwrap();
+        // Casting a small number from U512 to U256, should work.
+        let u256 = U256::try_from(u512).unwrap();
+        assert_eq!(u256, U256::from(42u64));
 
-    u512 += U512::ONE;
-
-    let u256: Result<U256, TryFromIntError> = TryFrom::<U512>::try_from(U512::MAX);
-    assert!(u256.is_err());
+        // Casting a big number (over the range of U256) from U512 to U256,
+        // should fail.
+        assert!(U256::try_from(U512::MAX).is_err());
+    }
 }
